34ff20687257f4a9a8583910989f6ec7a30d6092
Rubén Guadalupe Zavala Castañón <rubengpe.zavala@gmail.com>
2024-10-15 21:29:59 -0600

    seccion 6 calc app no solid

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
 //importacion
 import React from "react"
+import './App.css'
 
 //arrow function(ES6), () no recibe params
 //generacion de la funcion del componente
 const App = () => { 
     //cuerpo de la funcion, lo que ejecuta
     console.log("renderizacion de app")
-    return <h1>Calc App</h1>
+    return (
+        <main className="react-calculator">
+            <div className="result">
+            </div>
+            <div className="numbers">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>0</button>
+            </div>
+            <div className="functions">
+                <button>
+                    clear
+                </button>
+                <button>
+                    r
+                </button>
+            </div>
+            <div className="math-operations">
+                <button>+</button>
+                <button>-</button>
+                <button>*</button>
+                <button>/</button>
+                <button>=</button>
+            </div>
+        </main>
+    )
 }
 
 //exportacion, [default] es por que solo va a exportar un componente
